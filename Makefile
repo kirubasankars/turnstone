a9170a3ade8e2a92c11ca8da2403dd19e6afdb28
Kiruba Sankar Swaminathan <kiruba.sankars@gmail.com>
2026-01-24 15:08:26 -0500

    seperated generate-config from turnstone binary

--- Makefile
+++ Makefile
@@ -1,10 +1,11 @@
 build:
 	mkdir -p ./bin
-	go build -o bin/turnstone       cmd/turnstone/main.go
-	go build -o bin/turnstone-cli   cmd/turnstone-cli/main.go
+	go build -o bin/turnstone cmd/turnstone/main.go
+	go build -o bin/turnstone-cli cmd/turnstone-cli/main.go
+	go build -o bin/turnstone-generate-config cmd/turnstone-generate-config/main.go
 	go build -o bin/turnstone-bench cmd/turnstone-bench/main.go
-	go build -o bin/turnstone-load  cmd/turnstone-load/main.go
-	go build -o bin/turnstone-load2  cmd/turnstone-load2/main.go
+	go build -o bin/turnstone-load cmd/turnstone-load/main.go
+	go build -o bin/turnstone-load2 cmd/turnstone-load2/main.go
 	cd cmd/turnstone-duck && go mod tidy && go build -o ../../bin/turnstone-duck
 	cd cmd/turnstone-clickhouse && go mod tidy && go build -o ../../bin/turnstone-clickhouse
 
---
+++ cmd/turnstone-generate-config/main.go
@@ -0,0 +1,78 @@
+package main
+
+import (
+	"encoding/json"
+	"flag"
+	"fmt"
+	"os"
+	"path/filepath"
+	"strings"
+
+	"turnstone/config"
+	"turnstone/replication"
+)
+
+var (
+	homeDir  = flag.String("home", "tsdata", "Path to home directory to generate")
+	serverIP = flag.String("ip", "", "Comma-separated list of Server IPs/Hostnames to include in certificate (e.g. 192.168.1.10,myserver.local)")
+)
+
+func main() {
+	flag.Parse()
+
+	if *homeDir == "" {
+		fmt.Fprintln(os.Stderr, "Error: -home directory is required")
+		os.Exit(1)
+	}
+
+	fmt.Printf("Initializing TurnstoneDB home at: %s\n", *homeDir)
+
+	var extraHosts []string
+	if *serverIP != "" {
+		extraHosts = strings.Split(*serverIP, ",")
+		fmt.Printf("Adding subject alternative names: %v\n", extraHosts)
+	}
+
+	// 1. Define Default Server Config
+	defaultCfg := config.Config{
+		Port:                 ":6379",
+		Debug:                false,
+		MaxConns:             1000,
+		NumberOfDatabases:    4,
+		TLSCertFile:          "certs/server.crt",
+		TLSKeyFile:           "certs/server.key",
+		TLSCAFile:            "certs/ca.crt",
+		TLSClientCertFile:    "certs/client.crt",
+		TLSClientKeyFile:     "certs/client.key",
+		MetricsAddr:          ":9090",
+		WALRetention:         "2h",
+		WALRetentionStrategy: "replication",
+		BlockCacheSize:       "64MB",
+	}
+
+	configPath := filepath.Join(*homeDir, "turnstone.json")
+
+	// 2. Generate Artifacts (Directories, Certs, Server Config)
+	// Passed extraHosts as variadic arguments
+	if err := config.GenerateConfigArtifacts(*homeDir, defaultCfg, configPath, extraHosts...); err != nil {
+		fmt.Fprintf(os.Stderr, "Failed to generate artifacts: %v\n", err)
+		os.Exit(1)
+	}
+
+	// 3. Generate Default CDC Config
+	cdcCfg := replication.DefaultFileConsumerConfig()
+	cdcBytes, err := json.MarshalIndent(cdcCfg, "", "  ")
+	if err != nil {
+		fmt.Fprintf(os.Stderr, "Failed to marshal CDC config: %v\n", err)
+		os.Exit(1)
+	}
+
+	cdcConfigPath := filepath.Join(*homeDir, "turnstone.cdc.json")
+	if err := os.WriteFile(cdcConfigPath, cdcBytes, 0o644); err != nil {
+		fmt.Fprintf(os.Stderr, "Failed to write CDC config: %v\n", err)
+		os.Exit(1)
+	}
+
+	fmt.Printf("CDC configuration written to %s\n", cdcConfigPath)
+	fmt.Println("Initialization complete.")
+}
